b85ce574c8d6038222d1b6f4e34216259a72db93
Savelii <ajgixin3@gmail.com>
2026-04-18 18:11:51 +0200

    multi select with search to offer translation

--- src/app/component/offer-translation-page.ts
+++ src/app/component/offer-translation-page.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { NgOptionTemplateDirective, NgSelectComponent } from '@ng-select/ng-select';
+import { worldLanguages } from '../world-languages';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'offer-translation-page',
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MatButtonModule, NgOptionTemplateDirective, NgSelectComponent, FormsModule],
   templateUrl: './offer-translation-page.html',
   styleUrl: './offer-translation-page.css',
 })
-export class OfferTranslationPage {}
+export class OfferTranslationPage {
+  public readonly languages = worldLanguages;
+  public knownLanguages: string[] = [];
+}
