987f458a90771c71dd8aeb427a30c7203ba908a9
Savelii <ajgixin3@gmail.com>
2026-04-24 16:52:56 +0200

    find translator page with 2 select inputs

--- src/app/component/find-translator-page.ts
+++ src/app/component/find-translator-page.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { NgOptionTemplateDirective, NgSelectComponent } from '@ng-select/ng-select';
+import { worldLanguages } from '../world-languages';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'find-translator-page',
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MatButtonModule, NgOptionTemplateDirective, NgSelectComponent, FormsModule],
   templateUrl: './find-translator-page.html',
   styleUrl: './find-translator-page.css',
 })
-export class FindTranslatorPage {}
+export class FindTranslatorPage {
+  public readonly languages = worldLanguages;
+  public knownLanguages: string[] = [];
+  public requiredLanguages: string[] = [];
+}
