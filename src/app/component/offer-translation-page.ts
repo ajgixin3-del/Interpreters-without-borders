2c7504136db5eae883185fc3e90e48d7d557edae
Savelii <ajgixin3@gmail.com>
2026-04-25 18:21:59 +0200

    intagrated offer translation page with backend

--- src/app/component/offer-translation-page.ts
+++ src/app/component/offer-translation-page.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, Signal, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { NgOptionTemplateDirective, NgSelectComponent } from '@ng-select/ng-select';
-import { worldLanguages } from '../world-languages';
 import { FormsModule } from '@angular/forms';
+
+import { RtcOfferService } from '../service/rtc-offer-service';
+import { worldLanguages } from '../world-languages';
+import { MatchingStatus } from '../business-types';
 
 @Component({
   selector: 'offer-translation-page',
@@ -12,6 +15,24 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './offer-translation-page.css',
 })
 export class OfferTranslationPage {
+  @ViewChild('audioFromRemote') private audioPlayer!: ElementRef<HTMLAudioElement>;
+  private rtcService: RtcOfferService = inject(RtcOfferService);
+
   public readonly languages = worldLanguages;
+  public readonly MatchingStatus = MatchingStatus;
   public knownLanguages: string[] = [];
+
+  public triggerOfferTranslation(): void {
+    this.rtcService.triggerOfferTranslation(this.knownLanguages);
+
+    this.rtcService.listenAudioFromRemote((trackEvent: RTCTrackEvent) => {
+      if (this.audioPlayer.nativeElement.srcObject !== trackEvent.streams[0]) {
+        this.audioPlayer.nativeElement.srcObject = trackEvent.streams[0];
+      }
+    });
+  }
+
+  public get rtcEstablished(): Signal<MatchingStatus> {
+    return this.rtcService.rtcEstablished();
+  }
 }
