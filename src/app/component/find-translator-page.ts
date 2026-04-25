8ee18db0fb6bc28d67c9e9fa6923878c339aadea
Savelii <ajgixin3@gmail.com>
2026-04-25 18:44:22 +0200

    intagrated find translator page with backend

--- src/app/component/find-translator-page.ts
+++ src/app/component/find-translator-page.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, Signal, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { NgOptionTemplateDirective, NgSelectComponent } from '@ng-select/ng-select';
-import { worldLanguages } from '../world-languages';
 import { FormsModule } from '@angular/forms';
+
+import { worldLanguages } from '../world-languages';
+import { RtcAnswerService } from '../service/rtc-answer-service';
+import { MatchingStatus } from '../business-types';
 
 @Component({
   selector: 'find-translator-page',
@@ -12,7 +15,33 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './find-translator-page.css',
 })
 export class FindTranslatorPage {
+  @ViewChild('audioFromRemote') private audioPlayer!: ElementRef<HTMLAudioElement>;
+  private rtcService: RtcAnswerService = inject(RtcAnswerService);
+
   public readonly languages = worldLanguages;
+  public readonly MatchingStatus = MatchingStatus;
   public knownLanguages: string[] = [];
   public requiredLanguages: string[] = [];
+
+  public areLanguagesValid(): boolean {
+    return (
+      this.knownLanguages.length > 0 &&
+      this.requiredLanguages.length > 0 &&
+      this.knownLanguages.filter((x) => this.requiredLanguages.includes(x)).length == 0
+    );
+  }
+
+  public triggerFindTranslator(): void {
+    this.rtcService.triggerFindTranslator(this.knownLanguages, this.requiredLanguages);
+
+    this.rtcService.listenAudioFromRemote((trackEvent: RTCTrackEvent) => {
+      if (this.audioPlayer.nativeElement.srcObject !== trackEvent.streams[0]) {
+        this.audioPlayer.nativeElement.srcObject = trackEvent.streams[0];
+      }
+    });
+  }
+
+  public get rtcEstablished(): Signal<MatchingStatus> {
+    return this.rtcService.rtcEstablished();
+  }
 }
